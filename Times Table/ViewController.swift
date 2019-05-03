8bbb5ca18a6f6b0ba6b7771a0215a144fdc67748
AAJMvanMontfort <lout@me.com>
2019-05-03 09:27:56 +0200

    Initial Commit

---
+++ Times Table/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Times Table
+//
+//  Created by AAJM van Montfort on 03/05/2019.
+//  Copyright © 2019 AAJM van Montfort. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
