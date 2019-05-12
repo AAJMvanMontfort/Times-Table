cf9ab3eec8458dffcfede01973453474764e5f11
AAJMvanMontfort <lout@me.com>
2019-05-12 09:33:32 +0200

    Final version

--- Times Table/ViewController.swift
+++ Times Table/ViewController.swift
@@ -5,12 +5,33 @@
 //  Created by AAJM van Montfort on 03/05/2019.
 //  Copyright © 2019 AAJM van Montfort. All rights reserved.
 //
-//
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var timesSlider: UISlider!
+    @IBOutlet weak var timesTable: UITableView!
+
+    @IBAction func timesSliderChanged(_ sender: Any) {
+        
+        timesTable.reloadData()
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "resultCell")
+        
+        cell.textLabel?.text = String(Int(timesSlider.value * 20) * (indexPath.row + 1))
+        
+        return cell
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
